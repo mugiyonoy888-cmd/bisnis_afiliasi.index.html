9a80df8912f5cd72fa7e50703470ce276eb42434
mugiyonoy888-cmd <mugiyonoy888@gmail.com>
2026-07-10 19:38:05 +0700

    Update bisnis_afiliasi.py

--- bisnis_afiliasi.py
+++ bisnis_afiliasi.py
@@ -1,39 +1,3 @@
-import time
-
-def hitung_jeda_spesifik(kategori, urutan):
-    # 1. RUMUS JEDA BUDAYA
-    if kategori == "Budaya":
-        return (urutan // 2) + 2 if urutan % 2 != 0 else urutan // 2
-
-    # 2. RUMUS JEDA ARTIS
-    elif kategori == "Artis":
-        blok = (urutan - 1) // 5
-        posisi = (urutan - 1) % 5
-        deret_dasar = [5, 1, 4, 2, 3]
-        return deret_dasar[posisi] + (blok * 5)
-
-    # 3. RUMUS JEDA POLITIK
-    elif kategori == "Politik":
-        u = ((urutan - 1) % 10) + 1
-        return 11 - ((u + 1) // 2) if u % 2 != 0 else u // 2
-
-    # 4. RUMUS JEDA EKONOMI
-    elif kategori == "Ekonomi":
-        u = ((urutan - 1) % 20) + 1
-        pola_manual = [20, 1, 19, 2, 18, 3, 17, 3, 17, 4]
-        if u <= len(pola_manual):
-            return pola_manual[u - 1]
-        else:
-            return 21 - ((u + 1) // 2) if u % 2 != 0 else u // 2
-                
-    # 5. RUMUS JEDA YOUTUBE SHORTS
-    elif kategori == "YouTube Shorts":
-        u = ((urutan - 1) % 30) + 1
-        return 31 - ((u + 1) // 2) if u % 2 != 0 else u // 2
-                    
-    return 5
-
-
 def dapatkan_link_afiliasi(topik_konten):
     """
     LOGIKA AFILIASI ASLI: Robot mencocokkan kata kunci tren 
